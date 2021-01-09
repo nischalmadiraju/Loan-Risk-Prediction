b8752954818ffab161449eb5e053e15c580ce151
Nischal Madiraju <nischalmadhav@gmail.com>
2021-01-09 23:35:27 +0100

    adding necessary comments

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+#importing pandas library
 import pandas as pd
-from model import  train,predict
+
+#importing train and predict function from model.py
+from model import train, predict
+
+#import train_test_split method
 from sklearn.model_selection import train_test_split
+
+#import classification_report, confusion_matrix for calculating model's performance
 from sklearn.metrics import classification_report, confusion_matrix
 
-
 if __name__ == '__main__':
-
+    # Loading CSV File
     data = pd.read_csv('original.csv')
+    # Dropping rows with NaN values
     input = data.dropna()
+
+    # Splitting Dataset into train and test
     y = input['default']
     X = input.drop(columns=['default'])
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    print('hi')
-    model = train(X_train,y_train)
-    print('test')
+    # Training the model
+    model = train(X_train, y_train)
+    # making Prediction
     pred = predict(model, X_test)
     print(pred)
+
+    # Printing perfomance Metrics
     print(classification_report(y_test, pred))
     print('===================================')
     print(confusion_matrix(y_test, pred))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 from sklearn.linear_model import LogisticRegression
 
-
+#Function to train model
 def train(X_train, y_train):
     model = LogisticRegression()
     model.fit(X_train, y_train)
     return model
 
-
+#Fuction for prediction
 def predict(model, X_test):
     return model.predict(X_test)
